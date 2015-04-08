7e287a6cf945deab9c2e8f141674b87e936bf4b6
JoshuaSkootsky <joshua.skootsky@gmail.com>
2015-04-07 22:28:26 -0700

    Carrot pictures added

---
+++ carrot.py
@@ -0,0 +1,6 @@
+# Project: Is it a carrot?
+# Copyright Joshua Skootsky 2015
+# Distributed under the Boost Software License, Version 1.0
+# See LICENSE file or copy at http://www.boost.org/LICENSE_1_0.txt
+
+
